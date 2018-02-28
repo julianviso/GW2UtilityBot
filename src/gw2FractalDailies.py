95ca975ca04800694346cf8cd43a58166f86f96b
jviso <julian.viso@lexmark.com>
2018-02-28 10:02:38 -0500

    Simple bot will print out correct data while running

---
+++ src/gw2FractalDailies.py
@@ -0,0 +1,61 @@
+import discord 
+from discord.ext import commands
+from urllib.request import urlopen
+import json
+from pprint import pprint
+import secret
+
+bot = commands.Bot(command_prefix='-', description='testss')
+tomorrowsDailies = urlopen("https://api.guildwars2.com/v2/achievements/daily/tomorrow")
+data = json.load(tomorrowsDailies)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+
+@bot.command()
+async def theEternal():
+    await bot.say('sup Obi')
+
+@bot.command()
+async def info():
+    await bot.say('Bot created by Valrok. If you have feedback or suggestions on new features contact me on discord Valrok.3742 or in game.')
+
+@bot.command()
+async def helpMe():
+    await bot.say('```only current command: !tomorrowsFractals```')
+
+@bot.command()
+async def tomorrowsFractals():
+    results = []
+    formattedResults = None
+    for fractalData in data['fractals']:  
+        for ids in [fractalData['id']]:
+            readTomorrowsFractals = urlopen("https://api.guildwars2.com/v2/achievements?ids="+str(ids))
+            tomorrowsFractalsData = json.load(readTomorrowsFractals)
+            results.append(tomorrowsFractalsData[0]['name'])
+    #print ("\n".join(results))
+    formattedResults = "\n".join(results)
+    await bot.say(formattedResults)
+
+@bot.command()
+async def PlatMichelleT4s():
+    results = []
+    formattedResults = None
+    for fractalData in data['fractals']:  
+        for ids in [fractalData['id']]:
+            readTomorrowsFractals = urlopen("https://api.guildwars2.com/v2/achievements?ids="+str(ids))
+            tomorrowsFractalsData = json.load(readTomorrowsFractals)
+            results.append(tomorrowsFractalsData[0]['name'])
+    formattedResults = "\n".join(results)
+    await bot.say(formattedResults)
+
+displayName = urlopen("https://api.guildwars2.com/v2/achievements?ids=2327").read()
+itemReward = urlopen("https://api.guildwars2.com/v2/items?ids=68126").read()
+
+#print ("daily is: " + displayName)
+#print ("reward is: " + itemReward)
+bot.run(secret.client_secret)
+
+
