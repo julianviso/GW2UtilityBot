f68cb39c30e686b7c55150e434d5bbe68e6bc86a
julianviso <julian.viso@outlook.com>
2018-03-15 02:00:01 -0400

    Adding reddit commands using reddit's PRAW library

---
+++ src/reddit.py
@@ -0,0 +1,58 @@
+import discord
+from discord.ext import commands
+
+#Helper Libraries
+import feedparser
+import praw
+
+#Utils
+import strings
+import secret
+
+reddit = praw.Reddit(user_agent=secret.reddit_user_agent,
+    client_id=secret.reddit_id,
+    client_secret=secret.reddit_secret,
+    username=secret.reddit_username,
+    password=secret.reddit_password)
+
+class Reddit():
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def aww(self, ctx):
+        subreddit = reddit.subreddit('aww').random()
+        await self.bot.say(subreddit.url)
+
+    @commands.command(pass_context=True)
+    async def prequels(self, ctx):
+        subreddit = reddit.subreddit('PrequelMemes').random()
+        await self.bot.say(subreddit.url)
+
+    @commands.command(pass_context=True)
+    async def quotes(self, ctx):
+        subreddit = reddit.subreddit('quotes').random()
+        await self.bot.say(subreddit.title)
+
+    @commands.command(pass_context=True)
+    async def funny(self, ctx):
+        subreddit = reddit.subreddit('funny').random()
+        await self.bot.say(subreddit.url)
+
+    @commands.command(pass_context=True)
+    async def risky(self, ctx):
+        subreddit = reddit.subreddit('random').random()
+        await self.bot.say(subreddit.url)
+
+    @commands.command(pass_context=True, hidden=True)
+    async def liquid(self, ctx):
+        subreddit = reddit.subreddit('corgi').random()
+        await self.bot.say(subreddit.url)
+
+    @commands.command(pass_context=True)
+    async def dank(self, ctx):
+        subreddit = reddit.subreddit('dankmemes').random()
+        await self.bot.say(subreddit.url)
+
+def setup(bot):
+    bot.add_cog(Reddit(bot))
